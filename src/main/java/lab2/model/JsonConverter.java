0b5db8486cae82a1f680b68d4ed550d1afdd1715
Sh1ne <kosovchychserhiy@gmail.com>
2019-10-21 17:28:09 +0300

    Fixed Jackson dependency

---
+++ src/main/java/lab2/model/JsonConverter.java
@@ -0,0 +1,15 @@
+package lab2.model;
+
+public class JsonConverter<T> implements Serializable<T> {
+
+    @Override
+    public String serialize(T obj) {
+        return null;
+    }
+
+    @Override
+    public T deserialize(String str) {
+        return null;
+    }
+
+}
