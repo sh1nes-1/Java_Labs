e95080a5efb9b53dc7c87b730f553b7ab6a8f17c
Sh1ne <kosovchychserhiy@gmail.com>
2019-11-08 18:54:07 +0200

    Migrate project to Gradle

--- src/test/java/lab2/TestXmlConverter.java
+++ src/test/java/lab2/TestXmlConverter.java
@@ -8,13 +8,8 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import org.xmlunit.diff.DefaultNodeMatcher;
-import org.xmlunit.diff.ElementSelectors;
-import org.xmlunit.matchers.CompareMatcher;
 
 import java.time.LocalDate;
-
-import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestXmlConverter {
 
@@ -43,20 +38,9 @@ public class TestXmlConverter {
                 .setPrice(5500)
                 .build();
 
-        smartPhone2 = new SmartPhone.Builder()
-                .setId(2)
-                .setName("Xiaomi Redmi Note 7")
-                .setDiagonal(6.8)
-                .setColor(SmartPhone.Color.RED)
-                .setRam(4096)
-                .setReleaseDate(LocalDate.of(2019, 8, 16))
-                .setPrice(6800)
-                .build();
-
         // In catalog only item because items in catalog store in set (unordered) and after serialization order may be random
         catalog = new Catalog();
         catalog.addGoodsItem(smartPhone1, 5);
-        catalog.addGoodsItem(smartPhone2, 10);
     }
 
     @Test
@@ -82,15 +66,14 @@ public class TestXmlConverter {
 
     @Test
     public void serializeCatalogTest() throws ConvertException {
-        String expected = "<Catalog><SmartPhones><item><smartPhone><id>2</id><name>Xiaomi Redmi Note 7</name><price>6800</price><releaseDate>2019-08-16</releaseDate><color>RED</color><ram>4096</ram><diagonal>6.8</diagonal></smartPhone><amount>10</amount></item><item><smartPhone><id>1</id><name>Samsung Galaxy A30</name><price>5500</price><releaseDate>2019-06-15</releaseDate><color>BLACK</color><ram>3072</ram><diagonal>6.4</diagonal></smartPhone><amount>5</amount></item></SmartPhones></Catalog>";
+        String expected = "<Catalog><SmartPhones><item><smartPhone><id>1</id><name>Samsung Galaxy A30</name><price>5500</price><releaseDate>2019-06-15</releaseDate><color>BLACK</color><ram>3072</ram><diagonal>6.4</diagonal></smartPhone><amount>5</amount></item></SmartPhones></Catalog>";
         String actual = catalogXmlConverter.serializeToString(catalog);
-        assertThat(actual, CompareMatcher.isSimilarTo(expected)
-                .withNodeMatcher(new DefaultNodeMatcher(ElementSelectors.byNameAndText)));
+        Assert.assertEquals(actual, expected);
     }
 
     @Test
     public void deserializeCatalogTest() throws ConvertException {
-        String xmlString = "<Catalog><SmartPhones><item><smartPhone><id>2</id><name>Xiaomi Redmi Note 7</name><price>6800</price><releaseDate>2019-08-16</releaseDate><color>RED</color><ram>4096</ram><diagonal>6.8</diagonal></smartPhone><amount>10</amount></item><item><smartPhone><id>1</id><name>Samsung Galaxy A30</name><price>5500</price><releaseDate>2019-06-15</releaseDate><color>BLACK</color><ram>3072</ram><diagonal>6.4</diagonal></smartPhone><amount>5</amount></item></SmartPhones></Catalog>";
+        String xmlString = "<Catalog><SmartPhones><item><smartPhone><id>1</id><name>Samsung Galaxy A30</name><price>5500</price><releaseDate>2019-06-15</releaseDate><color>BLACK</color><ram>3072</ram><diagonal>6.4</diagonal></smartPhone><amount>5</amount></item></SmartPhones></Catalog>";
         Catalog actual = catalogXmlConverter.deserializeString(xmlString);
         Assert.assertEquals(actual, catalog);
     }
