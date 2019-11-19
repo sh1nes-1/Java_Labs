bead11d5e7ee1d51ee4be2846a178b846006cddf
Sh1ne <kosovchychserhiy@gmail.com>
2019-11-19 22:17:25 +0200

    Add my first working servlet with jsp page

---
+++ src/main/java/lab7/servlets/ShopServlet.java
@@ -0,0 +1,18 @@
+package lab7.servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ShopServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("views/shop.jsp");
+        requestDispatcher.forward(req, resp);
+    }
+
+}
